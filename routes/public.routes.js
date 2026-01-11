70e3ab0145aee8db4f272496739293c306cc3d5d
Sarthak <Sarthak9171>
2026-01-11 22:30:24 +0530

    Fix public join route to use queueId param

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { joinQueue, getTokenStatus } = require("../controllers/public.controller");
 
-router.post("/queues/join", joinQueue);
+router.post("/queues/:queueId/join", joinQueue);
 
 router.get("/queue/status", getTokenStatus);
 
